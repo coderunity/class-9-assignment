83c72405b3a765173edeeed6bafabe712ed34c3b
coderunity <coderunity@gmail.com>
2021-12-12 23:22:57 +0600

    upload all fiile

---
+++ app.js
@@ -0,0 +1,360 @@
+
+
+
+
+
+let gpa = [
+
+
+    {
+        id              : 01,
+        roll            : 01,
+        name            : 'MD Abu Raihan',
+        address         : 'Sherpur',
+        gender          : 'Male',
+        bn              :82,
+        en              :83,
+        math            :88,
+        s               :59,
+        ss              :79,
+        rel             :96,
+    },
+
+
+    {
+        id              : 02,
+        roll            : 02,
+        name            : 'MD Achem',
+        address         : 'Dhaka',
+        gender          : 'Male',
+        bn              :87,
+        en              :79,
+        math            :81,
+        s               :59,
+        ss              :86,
+        rel             :96,
+    },
+
+    {
+        id              : 03,
+        roll            : 03,
+        name            : 'MD Abu Sayeed',
+        address         : 'Comilla',
+        gender          : 'Male',
+        bn              :80,
+        en              :79,
+        math            :88,
+        s               :59,
+        ss              :79,
+        rel             :45,
+    },
+
+    {
+        id              : 04,
+        roll            : 04,
+        name            : 'MD Abu Sufiyan',
+        address         : 'Foridpur',
+        gender          : 'Male',
+        bn              :80,
+        en              :79,
+        math            :56,
+        s               :59,
+        ss              :79,
+        rel             :45,
+    },
+
+    {
+        id              : 05,
+        roll            : 05,
+        name            : 'MD Nur Amin',
+        address         : 'Bogra',
+        gender          : 'Male',
+        bn              :80,
+        en              :56,
+        math            :88,
+        s               :59,
+        ss              :79,
+        rel             :36,
+    },
+
+    {
+        id              : 06,
+        roll            : 06,
+        name            : 'MD Mamun',
+        address         : 'Rangpur',
+        gender          : 'Male',
+        bn              :80,
+        en              :56,
+        math            :88,
+        s               :59,
+        ss              :79,
+        rel             :36,
+    },
+
+    {
+        id              : 07,
+        roll            : 07,
+        name            : 'MD Somrat',
+        address         : 'Bogra',
+        gender          : 'Male',
+        bn              :80,
+        en              :56,
+        math            :88,
+        s               :59,
+        ss              :56,
+        rel             :36,
+    },
+
+    {
+        id              : 08,
+        roll            : 08,
+        name            : 'MD Sabbir',
+        address         : 'Rangpur',
+        gender          : 'Male',
+        bn              :80,
+        en              :79,
+        math            :56,
+        s               :59,
+        ss              :79,
+        rel             :36,
+    },
+
+    {
+        id              : 09,
+        roll            : 09,
+        name            : 'MD Nazmul',
+        address         : 'Comilla',
+        gender          : 'Male',
+        bn              :80,
+        en              :79,
+        math            :56,
+        s               :59,
+        ss              :79,
+        rel             :33,
+    },
+
+    {
+        id              : 10,
+        roll            : 10,
+        name            : 'MD Abdur Rohman',
+        address         : 'Sherpur',
+        gender          : 'Male',
+        bn              :80,
+        en              :56,
+        math            :88,
+        s               :59,
+        ss              :79,
+        rel             :56,
+    },
+
+
+];
+
+
+const phonenumber = [
+
+    {
+        id           : 01,
+        name         : 'Md Abu Raihan',
+        age          : 21,
+        phone        : '01771545594',
+        location     : 'Sherpur'
+
+    },
+
+    {
+        id           : 02,
+        name         : 'Abu Sufiyan',
+        age          : 27,
+        phone        : '0172489648',
+        location     : 'Dhaka'
+    },
+
+    {
+        id           : 03,
+        name         : 'MD Manum',
+        age          : 24,
+        phone        : '01771012790',
+        location     : 'Rangpur'
+    },
+
+    {
+        id           : 04,
+        name         : 'MD Nur Amin',
+        age          : 26,
+        phone        : '01785636965',
+        location     : 'Bogra'
+    },
+
+    {
+        id           : 05,
+        name         : 'MD Achem',
+        age          : 22,
+        phone        : '01754774203',
+        location     : 'Dhaka'
+    },
+
+    {
+        id           : 06,
+        name         : 'MD Somrat',
+        age          : 22,
+        phone        : '01726820147',
+        location     : 'Bogra'
+    },
+
+    {
+        id           : 07,
+        name         : 'MD Sabbir',
+        age          : 25,
+        phone        : '01303483202',
+        location     : 'Rangpur'
+    },
+
+    {
+        id           : 07,
+        name         : 'MD Abdur Rohman',
+        age          : 16,
+        phone        : '01773583320',
+        location     : 'Bagra'
+    }
+
+
+
+];
+
+const developer = [
+
+    {
+        id          : 01,
+        Name        : 'Abu Raihan',
+        Age         : 21,
+        Skill       : 'MERN stack',
+        Income      : 1500,
+        Location    : 'Sherpur'
+
+    },
+
+    {
+        id          : 02,
+        Name        : 'MD Achem',
+        Age         : 23,
+        Skill       : 'Javascrpt',
+        Income      : 1300,
+        Location    : 'Dhaka'
+
+    },
+
+    {
+        id          : 03,
+        Name        : 'MD Nur Amin',
+        Age         : 25,
+        Skill       : 'Laravel',
+        Income      : 1200,
+        Location    : 'Bogra'
+
+    },
+
+    {
+        id          : 04,
+        Name        : 'MD Mamun',
+        Age         : 23,
+        Skill       : 'Javascrpt',
+        Income      : 1200,
+        Location    : 'Rangpur'
+
+    },
+
+    {
+        id          : 05,
+        Name        : 'MD Somrat',
+        Age         : 22,
+        Skill       : 'Laravel',
+        Income      : 1000,
+        Location    : 'Dupchaciya'
+
+    },
+
+    {
+        id          : 06,
+        Name        : 'MD Sabbir',
+        Age         : 24,
+        Skill       : 'Golan',
+        Income      : 1100,
+        Location    : 'Rangpur'
+
+    },
+
+    {
+        id          : 07,
+        Name        : 'MD Abu Sufiyan',
+        Age         : 26,
+        Skill       : 'Django',
+        Income      : 1300,
+        Location    : 'Dhaka'
+
+    },
+
+    {
+        id          : 08,
+        Name        : 'Md Abu Syaeed',
+        Age         : 23,
+        Skill       : 'Javascrpt',
+        Income      : 1200,
+        Location    : 'Comilla'
+
+    },
+
+    {
+        id          : 09,
+        Name        : 'Md Nazmul',
+        Age         : 22,
+        Skill       : 'WordPress',
+        Income      : 900,
+        Location    : 'Comilla'
+
+    },
+
+    {
+        id          : 10,
+        Name        : 'MD Abdur Rohman',
+        Age         : 20,
+        Skill       : 'java',
+        Income      : 1300,
+        Location    : 'Dhaka'
+
+    }
+
+];
+
+
+const Gpaobject = JSON.stringify(gpa);
+
+localStorage.setItem('student',Gpaobject)
+
+
+const Phonenumberobject = JSON.stringify(phonenumber);
+
+localStorage.setItem('Family Data',Phonenumberobject)
+
+
+const Developerobject = JSON.stringify(developer);
+
+localStorage.setItem('Developer Data',Developerobject)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// git remote add origin https://github.com/coderunity/class-9-assignment.git
+// git branch -M main
+// git push -u origin main
+
+
